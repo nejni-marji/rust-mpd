51f10d1fa70d499ad4684bd4d689119d5be78c5a
nejni-marji <nejni.marji@gmail.com>
2025-04-02 21:33:11 -0500

    properly return an err

--- src/client.rs
+++ src/client.rs
@@ -679,7 +679,7 @@ impl<S: Read + Write> Proto for Client<S> {
     fn read_pair(&mut self) -> Result<(String, String)> {
         let line = self.read_line()?;
         if line.starts_with("ACK") {
-            return None.ok_or(ParseError::BadPair)?;
+            return Err(ParseError::BadPair.into());
         }
         let mut split = line.split(": ");
         let key = split.next().ok_or(ParseError::BadPair)?;
